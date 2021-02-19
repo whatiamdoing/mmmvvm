7333f0bbdb99b565dff2e65e92ed6d8e845fdde0
whatiamdoing <alwayswannafly070400@mail.ru>
2021-02-19 18:18:35 +0300

    Created empty project

---
+++ app/src/main/java/ru/whatiamdoing/mmmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.whatiamdoing.mmmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
